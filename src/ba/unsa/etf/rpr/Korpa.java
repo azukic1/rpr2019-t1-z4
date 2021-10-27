daf007fa5407f33dcdf3be5a6fa16ea9b1c977b9
Ajla Zukic <azukic1@etf.unsa.ba>
2021-10-27 21:53:23 +0200

    Implementovane klase Artikl, Korpa i Supermarket

--- src/ba/unsa/etf/rpr/Korpa.java
+++ src/ba/unsa/etf/rpr/Korpa.java
@@ -1,4 +1,42 @@
 package ba.unsa.etf.rpr;
 
 public class Korpa {
+    private Artikl[] artikli = new Artikl[50];
+    private int brojArtikala = 0;
+    public boolean dodajArtikl(Artikl noviArtikl) {
+        if(brojArtikala<50){
+            artikli[brojArtikala] = new Artikl(noviArtikl.getNaziv(),noviArtikl.getCijena(),noviArtikl.getKod());
+            brojArtikala++;
+            return true;
+        }
+        return false;
+    }
+
+    public Artikl[] getArtikli() {
+        return artikli;
+    }
+
+    public Artikl izbaciArtiklSaKodom(String kod) {
+        for(int i=0; i<brojArtikala; i++) {
+            if(kod.equals(artikli[i].getKod())) {
+                Artikl artiklZaIzbacivanje = artikli[i];
+                for (int j = i; j + 1 < brojArtikala; j++) {
+                    artikli[j] = artikli[j + 1];
+                }
+                brojArtikala--;
+                artikli[brojArtikala] = null;
+                return artiklZaIzbacivanje;
+            }
+
+        }
+        return null;
+    }
+
+    public int dajUkupnuCijenuArtikala() {
+        int ukupnaCijena = 0;
+        for(int i=0; i<brojArtikala; i++) {
+            ukupnaCijena = ukupnaCijena + artikli[i].getCijena();
+        }
+        return ukupnaCijena;
+    }
 }
--- src/ba/unsa/etf/rpr/Supermarket.java
+++ src/ba/unsa/etf/rpr/Supermarket.java
@@ -1,4 +1,35 @@
 package ba.unsa.etf.rpr;
 
 public class Supermarket {
+    private Artikl[] artikli = new Artikl[50];
+    private int brojArtikala = 0;
+
+    public void dodajArtikl(Artikl noviArtikl) {
+        if(brojArtikala<50){
+            artikli[brojArtikala] = new Artikl(noviArtikl.getNaziv(),noviArtikl.getCijena(),noviArtikl.getKod());
+            brojArtikala++;
+
+        }
+
+    }
+
+    public Artikl[] getArtikli() {
+        return artikli;
+    }
+
+    public Artikl izbaciArtiklSaKodom(String kod) {
+        for(int i=0; i<brojArtikala; i++) {
+            if(kod.equals(artikli[i].getKod())) {
+                Artikl artiklZaIzbacivanje = artikli[i];
+                for (int j = i; j + 1 < brojArtikala; j++) {
+                    artikli[j] = artikli[j + 1];
+                }
+                brojArtikala--;
+                artikli[brojArtikala] = null;
+                return artiklZaIzbacivanje;
+            }
+
+        }
+        return null;
+    }
 }
